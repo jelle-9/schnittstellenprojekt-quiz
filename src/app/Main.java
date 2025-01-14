fb172195b09939a3befb5edcf285fe82cc78cade
Jamal Helle <jamaluddin.helle@gmail.com>
2025-01-14 12:10:54 +0100

    QuizSession hinzugefügt und in Main integriert
    Die Klasse QuizSession verwaltet den Quiz-Ablauf. Main.java wurde für die Integration angepasst, test-fragen wurden hinzugefügt. Grundlage für eine Quiz-App mit Observer-Support geschaffen.

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,7 +1,29 @@
 package app;
 
+import model.QuestionFactory;
+import observer.*;
+import service.QuizSession;
+
 public class Main {
     public static void main(String[] args) {
 
+        QuizNotifier notifier = new QuizNotifier();
+
+        QuizSession session = new QuizSession();
+        session.addObserver(notifier);
+
+        //Test-Fragen
+        session.addQuestion(QuestionFactory.createMultipleChoiceQuestion(
+                "Was ist die Standard-Datenstruktur, die in Java verwendet wird, um eine geordnete Sammlung von Elementen zu speichern?",
+                new String[]{"List", "Set", "Queue", "Map"},
+                "1"
+        ));
+        session.addQuestion(QuestionFactory.createMultipleChoiceQuestion(
+                "Welches SQL-Statement wird verwendet, um Daten in eine Tabelle einzufügen?",
+                new String[]{"SELECT", "INSERT", "DELETE", "UPDATE"},
+                "2"
+        ));
+
+        session.start();
     }
 }
